655f21de66fd3962cd507a22efab8e7d5587ef9b
fabianrmz <A01630024@itesm.mx>
2018-04-25 21:18:22 -0500

    w

--- src/HowToPlayWindow.java
+++ src/HowToPlayWindow.java
@@ -7,19 +7,20 @@ import javax.swing.JPanel;
 
 public class HowToPlayWindow extends JPanel{
 	private Image background;
+	private Image title;
 	
 	public HowToPlayWindow() {
 		super();
 		this.setPreferredSize(new Dimension(600,600));
 		this.background=new ImageIcon("howtoplay.gif").getImage();
-		
+		this.background=new ImageIcon("instructionstitle.png").getImage();
 	}
 	
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage(this.background, 0, 0, 972, 622, this);
-		
+		g.drawImage(this.title, 0, 0,1,100, this);
 	}
 
 }
