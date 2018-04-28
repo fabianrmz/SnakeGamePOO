63f2074dada913d2229883ee2a776ed910da2fdb
fabianrmz <A01630024@itesm.mx>
2018-04-28 15:25:00 -0500

    ya

--- src/StartGame.java
+++ src/StartGame.java
@@ -13,7 +13,7 @@ public class StartGame extends JFrame{
 		Puntaje punt=new Puntaje(game);
 		this.add(game);
 		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setLocation( (pantalla.width - 800) / 2, (pantalla.height - 600) / 2);
+		this.setLocation( (pantalla.width - 600) / 2, (pantalla.height - 800) / 2);
 		this.setResizable(false);
 		this.pack();
 		this.setVisible(true);
